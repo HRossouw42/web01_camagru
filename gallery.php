b20ab4452bdbbe4843b47e8f3c195f77042710b4
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-12-19 12:45:23 +0200

    user gallery delete images added

--- gallery.php
+++ gallery.php
@@ -91,13 +91,47 @@
     <section class="hero">
     <section class="section has-background-light">
         <?php
-        $out2 = $db->returnRecord("SELECT * FROM images ORDER BY `date` DESC");
-        //            print_r($out2);
-        $i = 0;
-        while ($out2[$i]){
-            echo "<img src=".$out2[$i]["image"].">";
-            $i++;
-        }
+                //debug code
+//            $out2 = $db->returnRecord("SELECT * FROM images ORDER BY `date` DESC");
+//            //            print_r($out2);
+//            $i = 0;
+//            while ($out2[$i]){
+//                echo "<img src=".$out2[$i]["image"].">";
+//                $i++;
+//            }
+
+            //delete button
+            if ($_POST){
+                $num = array_search('Delete Image', $_POST);
+                $statement = "DELETE FROM `images` WHERE imageID = ".toQuote($num);
+                $db->runStatement($db->getDBConn(),$statement);
+            }
+
+            $imagelimit = 5;
+            $out2 = $db->returnRecord("SELECT * FROM images WHERE username = ".toQuote($_SESSION["username"]));
+            $total = count($out2);
+            if(isset($_GET["page"])){
+                $page = $_GET["page"];
+                $i = ($_GET["page"] - 1 )* $imagelimit;
+            }
+            else{
+                $i = 0;
+                $page = 1;
+            }
+            $pages = ceil($total / $imagelimit);
+
+            echo "<div class='galdiv' style='top:120%'>";
+                while ($i < $imagelimit*$page && $out2[$i]){
+                echo "<div class='imagediv'><img src=".$out2[$i]["image"]."></div>";
+                echo "<br><form method='post' action=''><input type='submit' name='".$out2[$i]["imageID"]."' class='btn1 button' value='Delete Image'></form>";
+                $i++;
+                }
+                echo "<br><div class='imagediv' style='bottom:0%'>";
+                    for ($x = 1; $x <= $pages; $x++){
+                    echo "<a href='gallery.php?page=$x'>$x</a>"."\t";
+                    }
+                    echo "</div>";
+                echo "</div>";
         ?>
     </section>
     </section>
